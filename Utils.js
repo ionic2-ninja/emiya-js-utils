d04fa7e54e5fd45fd0cd47c16b516eed2c750bf8
EmiyaLee <370489964@qq.com>
2017-02-15 13:24:30 +0800

    add sourceMap

--- Utils.js
+++ Utils.js
@@ -1,4 +1,5 @@
 'use strict';
+exports.__esModule = true;
 var Utils = (function () {
     function Utils() {
     }
@@ -385,3 +386,4 @@ var Utils = (function () {
     return Utils;
 }());
 exports.Utils = Utils;
+//# sourceMappingURL=Utils.js.map
